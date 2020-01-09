5fdb86d9c8552f71ddf750fcff932c0232ff4480
Sean Lim <seanlimdev856@gmail.com>
2020-01-09 16:20:33 +0800

    Creat enrolment flow

--- src/views/Enrolment.svelte
+++ src/views/Enrolment.svelte
@@ -1,12 +1,20 @@
 <script>
 import Webcam from 'webcamjs';
 import { onMount } from 'svelte';
+import { tweened } from 'svelte/motion';
+import { cubicOut } from 'svelte/easing';
+
+const progress = tweened(0, {
+		duration: 400,
+		easing: cubicOut
+});
 
 let name = '',
     email = '',
-    error = '';
-
-let cameraActive = false;
+    pictures = [],
+    error = '',
+    instructions = 'Make sure the front of your face can be seen, and then press start.',
+    cameraActive = false;
 
 $: detailsValid = () => {
   if (name === '' || email === '') {
@@ -26,7 +34,7 @@ function validateEmail(email) {
     return re.test(String(email).toLowerCase());
 }
 
-function attach() {      
+function attachCamera() {      
   if (detailsValid()){
     Webcam.set({
         width: 1280,
@@ -39,10 +47,27 @@ function attach() {
   }
 }
 
-function snapshot() {
-  Webcam.snap((data) => {
-    console.info(data);
-  });
+function startCapture() {
+  const bufferTime = 2000;
+  const numberOfImages = 15;
+  let captureInterval = setInterval(()=> {
+    Webcam.snap(console.info);
+    progress.set((pictures.length / numberOfImages));
+
+    if (pictures.length >= 5) instructions = 'Turn slightly to the right...';
+    if (pictures.length >= 10) instructions = 'Turn slightly to the left...';
+
+    // Terminate 
+    if (pictures.length >= numberOfImages) {
+      clearInterval(captureInterval);
+      enrolAttendee();
+    }
+
+  }, bufferTime);
+}
+
+function enrolAttendee() {
+  // TODO: Make request to enrol attendee
 }
 </script>
 
@@ -63,13 +88,15 @@ input {
   <h2>Enrolment</h2>
   <div id="my_camera"> </div>
   {#if cameraActive}
-    <div class="button" on:click={snapshot}> Capture </div>
+    <p> {instructions} </p>
+    <progress value={$progress}></progress>
+    <div class="button" on:click={startCapture}> Start </div>
   {:else}
     <p> To get started, enter your details </p>
     <input disabled="{cameraActive}" name="name" type="text" placeholder="Name" bind:value={name}> <br/>
     <input disabled="{cameraActive}" name="email" type="text" placeholder="Email" bind:value={email}> <br/>
     <p style="color: red;">{error}</p>
-    <div class="button" on:click={attach}>Begin</div>  
+    <div class="button" on:click={attachCamera}>Begin</div>  
   {/if}
 </div>
 
